b2efb25c133c3f028ff351a14ff70c93e63240dc
maxence30 <henous.maxence@outlook.fr>
2026-06-15 15:19:20 +0200

    test 2 task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-"""import des fonctions Asyincio"""
+"""This module contains an asynchronous function that waits for a random delay."""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Attend un délai aléatoire puis le retourne."""
+    """Wait for a random delay and return the delay value."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
