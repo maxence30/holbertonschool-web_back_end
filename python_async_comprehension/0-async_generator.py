bb0763f9762283351ce0a7ebb26126aacd1e3779
maxence30 <henous.maxence@outlook.fr>
2026-06-15 15:12:14 +0200

    task 0 :  The basics of async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+"""import des fonctions Asyincio"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Attend un délai aléatoire puis le retourne."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
