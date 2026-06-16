1f1d27fee10f9ff0bac7c41bd60c7dd6a7ffadd2
maxence30 <henous.maxence@outlook.fr>
2026-06-16 15:33:55 +0200

    FINISH TASK 0 PROJECT Python - Async Comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import asyncio
+import random
 
-async def async_generator()
-    await asyncio.sleep(10)
+async def async_generator():
+    for i in range(10):
+    await asyncio.sleep(1)
+    yield random.uniform (0, 10)
+    
