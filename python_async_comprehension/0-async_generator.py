b4051afe809844ecec6cbeba1455d4c4b1641577
maxence30 <henous.maxence@outlook.fr>
2026-06-16 15:38:40 +0200

    test v2 task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""Ce module définit une coroutine qui génère des nombres aléatoires de manière asynchrone."""
+
 import asyncio
 import random
 
-async def async_generator():
-    for i in range(10):
+
+async def async_generator()
+"""Génère de manière asynchrone 10 nombres aléatoires entre 0 et 10 avec une pause d'une seconde."""
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform (0, 10)
+        yield random.uniform(0, 10)
     
