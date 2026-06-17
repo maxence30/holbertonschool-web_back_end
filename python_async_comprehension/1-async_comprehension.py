c6968e03abe968bfd05f7996383595416a1782dd
maxence30 <henous.maxence@outlook.fr>
2026-06-17 14:16:18 +0200

    task 1 project Python - Async Comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Module contenant une coroutine utilisant une compréhension asynchrone."""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """Collecte les valeurs produites par async_generator."""
+    return [i async for i in async_generator()]
