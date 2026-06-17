62643a9360f51b42a0063084b4d64fd42eb2608d
maxence30 <henous.maxence@outlook.fr>
2026-06-17 14:24:54 +0200

    task 2 project Python - Async Comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""Module mesuring the runtime of async comprehensions."""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute four async comprehensions in parallel and return runtime."""
+    start = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end = time.perf_counter()
+
+    return end - start
